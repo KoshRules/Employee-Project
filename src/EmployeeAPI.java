3618fb1f768a9b4f449b7c96e393e7aed16e903e
Daniel Wilson <valen0866@msn.com>
2020-12-19 00:36:03 +0000

    making changes

--- src/EmployeeAPI.java
+++ src/EmployeeAPI.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class EmployeeAPI
 {
@@ -74,12 +75,31 @@ public class EmployeeAPI
         }
     }
 
+    /**
+     * @param manager builds and returns a list of employees(and their associated index num in the arraylist)
+     *                by their manager. each on a new line
+     * @return this manager does not exist; if the manager doesnt exist
+     *          This manager has no employees in their department; if the manager exists, but has no employees
+     *                                                              in their department
+     */
     //TODO
     public String listManagerEmployees(Manager manager) {return null;}
 
+    /**
+     * builds and returns a list of employees who are managers
+     * @return No Managers in the system; if there are no anagers in the system
+     */
     //TODO
-    public String listManagerEmployees() {return null;}
+    public String listManagerEmployees()
+    {
+        return null;
+    }
 
+    /**
+     * @param index searches through the employees collection for the employee with the supplied second name
+     * @return the position of the employee; if employee exists
+     * otherwise return -1
+     */
     //TODO
     //public int searchEmployees(String index) {return null;}
 
@@ -89,17 +109,36 @@ public class EmployeeAPI
     //TODO
     //public double avgSalariesOwed() {return null;}
 
+    /**
+     * calculates the employee with the highest (total) salary
+     * @return employee object of that employee
+     *          null; if no employees
+     */
     //TODO
     public Employee employeeWithHighestPay() {return null;}
 
+    /**
+     * sorts the employees object in ascending alphabetical order (firstname) of employees
+     */
     //TODO
-    public void sortEmployeesByFirstName() {}
+    public void sortEmployeesByFirstName(){Collection.sort(employees);}
+
+    /**
+     * sorts the employees object in ascending alphabetical order (secondname) of employees
+     */
+    //TODO
+    public void sortEmployeesBySecondName(){
+        Collection.sort(employees);
+    }
 
     //TODO
-    public void sortEmployeesBySecondName() {}
-
-    //TODO
-    private void swapEmployees(ArrayList<Employee> employees, int x, int y) {}
+    private void swapEmployees(ArrayList<Employee> employees, int x, int y)
+    {
+        //get list of all employees
+        //take first name index num to be swapped
+        //take second name index num and switch with first index num
+        //display updated employee list
+    }
 
     //TODO
     public void load() {}
